3b88ba9bbdea3e408a5022ef73a43fbc8341e8bc
Franck Kamenan <franck.kamenan@yahoo.com>
2020-10-24 13:14:30 +0200

    string array

---
+++ Main.java
@@ -0,0 +1,15 @@
+package be.intecbrussel.streaming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+	String[] words = {"Hello", "this", "is", "a", "stream"};
+
+        for (String i: words) {
+
+            System.out.println(i);
+        }
+    }
+}
